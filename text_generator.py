5bcf9c1f0a2d838c77757b62c3973157643b23d9
ramzi12rtx <ughggvbg@gmail.com>
2025-08-02 22:05:22 +0100

    Create text_generator.py

---
+++ text_generator.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_text():
+    texts = [
+        "Did you know? Honey never spoils.",
+        "Focus on your goals, not your fears.",
+        "Drink water first thing in the morning.",
+        "One minute of anger weakens your immunity for 4-5 hours."
+    ]
+    return random.choice(texts)
